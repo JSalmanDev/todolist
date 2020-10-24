5c9f90d049c6c6456abfaea9fede04df19593db7
JSalmanDev <jamil@isalman.net>
2020-10-24 09:32:59 +0500

    navbar added

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { withRouter } from 'react-router-dom';
+
+const NavBar = (props) => {
+
+    const gotoRoute = (e, endpoint) => {
+        e.preventDefault();
+        props.history.push(endpoint);    
+    }
+
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Nav className="mr-auto">
+                <Nav.Link href="/"  onClick={(e) => gotoRoute(e, '/')}>About</Nav.Link>
+                <Nav.Link href="/todos" onClick={(e) => gotoRoute(e, '/todos')}>Todos</Nav.Link>
+                <Nav.Link href="/contact" onClick={(e) => gotoRoute(e, '/contact')}>Contact</Nav.Link>
+            </Nav>
+        </Navbar>
+    );
+};
+
+export default withRouter(NavBar);
