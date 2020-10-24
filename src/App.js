56e22ab8ecbd7cb094ceaf5cb2f04c013588d53e
JSalmanDev <jamil@isalman.net>
2020-10-24 09:34:46 +0500

    routing using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import { Switch, Route } from 'react-router-dom';
+import Todos from './components/Todos';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Switch>
+        <Route exact path={"/todos"} component={Todos} />
+        <Route exact path={"/"} component={About} />
+        <Route exact path={"/contact"} component={Contact} />  
+      </Switch>      
     </div>
   );
 }
